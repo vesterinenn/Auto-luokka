610852059149d518eb20500aa0238b6f1cecc3d9
Sara Vesterinen <sara.vesterinen@hotmail.com>
2021-11-24 12:30:55 +0200

    Metodit
    Jarruta, kiihdyta, naytaTiedot

--- src/AutoMain.java
+++ src/AutoMain.java
@@ -13,4 +13,22 @@ class Auto{
 	public String merkki, malli;
 	public int bensanMaara;
 	
-}
+	public void jarruta()
+	{
+		System.out.println("Auto jarruttaa");
+	}
+	
+	public void kiihdyta()
+	{
+		bensanMaara--;
+		System.out.println("Auto kiihtyy");
+	}
+	
+	public void naytaTiedot()
+	{
+		System.out.println("Merkki: " + merkki);
+		System.out.println("Malli: " + malli);
+		System.out.println("Bensan m‰‰r‰: " + bensanMaara);
+	}
+	
+} //Auto-luokka loppuu
